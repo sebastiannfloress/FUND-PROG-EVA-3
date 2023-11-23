65dbb2627104cef90d59618ef92ee784c9c89537
sebastiannfloress <143568236+sebastiannfloress@users.noreply.github.com>
2023-11-22 21:24:24 -0700

    Add files via upload

---
+++ EVA3_1_FUNCIONES/src/main/java/com/mycompany/eva3_1_funciones/EVA3_1_FUNCIONES.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3_1_funciones;
+
+/**
+ *
+ * @author saulf
+ */
+public class EVA3_1_FUNCIONES {
+
+    public static void main(String[] args) {
+        //LLAMADA A FUNCION 
+        imprimirMensaje("HOLA MUNDO!!!");
+        
+    }
+    
+    public static void imprimirMensaje(String mensaje){
+        System.out.println(mensaje);
+    }
+}
