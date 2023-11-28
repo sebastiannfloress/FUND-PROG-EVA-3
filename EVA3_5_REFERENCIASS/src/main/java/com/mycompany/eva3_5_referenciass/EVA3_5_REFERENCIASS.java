8de2289327bb8215ae1707d663ef025aea7df98b
sebastiannfloress <143568236+sebastiannfloress@users.noreply.github.com>
2023-11-27 20:14:53 -0700

    Add files via upload

---
+++ EVA3_5_REFERENCIASS/src/main/java/com/mycompany/eva3_5_referenciass/EVA3_5_REFERENCIASS.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3_5_referenciass;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author saulf
+ */
+public class EVA3_5_REFERENCIASS {
+
+    public static void main(String[] args) {
+        int[] arreglo = new int [10];
+        System.out.println(arreglo);
+        for ( int i : arreglo){
+            System.out.println(i);
+        }
+        String[][]arreCade = new String[10][10];
+        System.out.println(arreCade);
+        Scanner input = new Scanner(System.in);
+        System.out.println(input);
+    }
+}
